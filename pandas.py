613951b78e889553da771e09b88de06a4a75ccf2
adammcphail12 <99302328+adammcphail12@users.noreply.github.com>
2023-02-17 12:30:10 +1300

    Add files via upload

---
+++ pandas.py
@@ -0,0 +1,12 @@
+import pandas
+# dictionaries to hold ticket details.
+
+all_names = ['a','b','c','d','e']
+all_tickets_costs = [7.50,7.50,10.50,10.50,6.50]
+surcharge = [0,0,0.53,0.53,0]
+
+
+mini_movie_dict = {
+    "Name" = all_names,
+    'Ticket Price' = 
+}
