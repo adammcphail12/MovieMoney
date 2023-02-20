bacdeea81ca1f52f1a5f022a94e145e2e47b4c27
Adam McPhail <99302328+adammcphail12@users.noreply.github.com>
2023-02-20 20:28:00 +0000

    renamed pandas file to MMF_pandas

---
+++ MMF_pandas.py
@@ -0,0 +1,22 @@
+import pandas
+# dictionaries to hold ticket details.
+
+all_names = ['a','b','c','d','e']
+all_tickets_costs = [7.50,7.50,10.50,10.50,6.50]
+surcharge = [0,0,0.53,0.53,0]
+
+
+mini_movie_dict = {
+    'Name' : all_names,
+    'Ticket Price' : all_tickets_costs,
+    'Surcharge' : surcharge
+}
+
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+
+#calulate total cost (ticket + surcharge)
+mini_movie_frame['Total'] = (mini_movie_frame['Surcharge'] +
+mini_movie_frame['Ticket Price'])
+
+
+print(mini_movie_frame)
